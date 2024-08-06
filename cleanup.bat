a3f480e61e8d1e1d8a40e2be8ac79cbd10014ced
Kevin Weldon <kevin.weldon@intel.com>
2024-08-06 13:41:01 -0400

    Commit

--- cleanup.bat
+++ cleanup.bat
@@ -1,9 +1,11 @@
 @echo off
 rmdir qdb tmp-clearbox software DNI /s /q
 rmdir .qsys_edit ip sys sandboxes /s /q
+rmdir db /s /q
 del *.rpt *.sof *.summary *.smsg *.pin *~ /s
 del *.qsf *.qpf *.qws *.v *.sv *.sdc *.done *.qsys /s
 del *.cdf *.sld *.qarlog *.legacy *.xml *.info /s
 del *.json *.qdf #*# *.mif *.log *.rec ~* /s
+del *.save /s
 
 
